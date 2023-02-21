3d0e8fac00a01dfcd668ddaa5ab6e0db98197431
Jolan Plaum <jolan.plaum@student.howest.be>
2023-02-21 21:29:01 +0100

    Create Component classes

---
+++ Minigin/Component.h
@@ -0,0 +1,64 @@
+#pragma once
+
+namespace dae
+{
+	//=============================
+	// Base Component Class
+	//=============================
+	class Component
+	{
+	public:
+		// Constructors and Destructor
+		Component() = default;
+		virtual ~Component() = default;
+
+		// Copy and Move semantics
+		Component(const Component& other)					= delete;
+		Component& operator=(const Component& other)		= delete;
+		Component(Component&& other) noexcept				= delete;
+		Component& operator=(Component&& other) noexcept	= delete;
+
+		// Public Member Functions
+		virtual void Init() {};
+	};
+
+	//=============================
+	// Render Component Class
+	//=============================
+	class RenderComponent : public Component
+	{
+	public:
+		// Constructors and Destructor
+		RenderComponent() = default;
+		virtual ~RenderComponent() = default;
+
+		// Copy and Move semantics
+		RenderComponent(const RenderComponent& other)					= delete;
+		RenderComponent& operator=(const RenderComponent& other)		= delete;
+		RenderComponent(RenderComponent&& other) noexcept				= delete;
+		RenderComponent& operator=(RenderComponent&& other) noexcept	= delete;
+
+		// Public Member Functions
+		virtual void Render() const = 0;
+	};
+
+	//=============================
+	// Behaviour Component Class
+	//=============================
+	class BehaviourComponent : public Component
+	{
+	public:
+		// Constructors and Destructor
+		BehaviourComponent() = default;
+		virtual ~BehaviourComponent() = default;
+
+		// Copy and Move semantics
+		BehaviourComponent(const BehaviourComponent& other)					= delete;
+		BehaviourComponent& operator=(const BehaviourComponent& other)		= delete;
+		BehaviourComponent(BehaviourComponent&& other) noexcept				= delete;
+		BehaviourComponent& operator=(BehaviourComponent&& other) noexcept	= delete;
+
+		// Public Member Functions
+		virtual void Update() = 0;
+	};
+}
