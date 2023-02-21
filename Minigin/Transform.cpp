8f631d2882aa1f477b101dc49c13c305bfc224ff
Jolan Plaum <jolan.plaum@student.howest.be>
2023-02-21 21:31:24 +0100

    Create Transform Component

--- Minigin/Transform.cpp
+++ Minigin/Transform.cpp
@@ -1,8 +1,67 @@
+//-----------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------
 #include "Transform.h"
 
-void dae::Transform::SetPosition(const float x, const float y, const float z)
+using namespace dae;
+
+
+//-----------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------
+Transform::Transform()
 {
-	m_position.x = x;
-	m_position.y = y;
-	m_position.z = z;
 }
+
+
+//-----------------------------------------------------------------
+// Destructor
+//-----------------------------------------------------------------
+Transform::~Transform()
+{
+}
+
+
+//-----------------------------------------------------------------
+// Public Member Functions
+//-----------------------------------------------------------------
+void Transform::SetPosition(const glm::vec3& pos)
+{
+	m_Position = pos;
+}
+
+void Transform::SetPosition(float x, float y, float z)
+{
+	SetPosition({ x, y, z });
+}
+
+void Transform::SetRotation(const glm::vec3& rot)
+{
+	m_Rotation = rot;
+}
+
+void Transform::SetRotation(float x, float y, float z)
+{
+	SetRotation({ x, y, z });
+}
+
+void Transform::SetScale(const glm::vec3& s)
+{
+	m_Scale = s;
+}
+
+void Transform::SetScale(float x, float y, float z)
+{
+	SetScale({ x, y, z });
+}
+
+void Transform::SetScale(float s)
+{
+	SetScale({ s, s, s });
+}
+
+
+//-----------------------------------------------------------------
+// Private Member Functions
+//-----------------------------------------------------------------
+
--- Minigin/Transform.h
+++ Minigin/Transform.h
@@ -1,14 +1,50 @@
 #pragma once
+// Includes
 #include <glm/glm.hpp>
+#include "Component.h"
 
 namespace dae
 {
-	class Transform final
+	// Class Forward Declarations
+	
+	// Class Declaration
+	class Transform final : public Component
 	{
 	public:
-		const glm::vec3& GetPosition() const { return m_position; }
+		// Constructors and Destructor
+		explicit Transform() = default;
+		~Transform() = default;
+		
+		// Copy and Move semantics
+		Transform(const Transform& other)					= default;
+		Transform& operator=(const Transform& other)		= default;
+		Transform(Transform&& other) noexcept				= default;
+		Transform& operator=(Transform&& other) noexcept	= default;
+	
+		//---------------------------
+		// Public Member Functions
+		//---------------------------
+		void SetPosition(const glm::vec3& pos);
 		void SetPosition(float x, float y, float z);
+		void SetRotation(const glm::vec3& rot);
+		void SetRotation(float x, float y, float z);
+		void SetScale(const glm::vec3& s);
+		void SetScale(float x, float y, float z);
+		void SetScale(float s);
+
+		const glm::vec3& GetPosition() const { return m_Position; }
+		const glm::vec3& GetPosition() const { return m_Position; }
+		const glm::vec3& GetPosition() const { return m_Position; }
+	
+	
 	private:
-		glm::vec3 m_position;
+		// Member variables
+		glm::vec3 m_Position, m_Rotation, m_Scale;
+	
+		//---------------------------
+		// Private Member Functions
+		//---------------------------
+	
 	};
 }
+
