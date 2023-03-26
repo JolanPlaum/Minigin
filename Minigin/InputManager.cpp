3f2c687cb21b908571fed88a663b1187f3fbf4a3
Jolan Plaum <jolan.plaum@student.howest.be>
2023-03-26 23:54:27 +0200

    Implement InputManger command functionality

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,25 +1,120 @@
-#include <SDL.h>
+//-----------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------
 #include "InputManager.h"
+#include <SDL.h>
+#include <Xinput.h>
 #include <backends/imgui_impl_sdl2.h>
 
-bool dae::InputManager::ProcessInput()
+using namespace dae;
+
+
+//-----------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------
+InputManager::InputManager()
+{
+	m_Keyboard = std::make_unique<Keyboard>();
+
+	m_Gamepads.reserve(XUSER_MAX_COUNT);
+	for (int i{}; i < XUSER_MAX_COUNT; ++i)
+	{
+		m_Gamepads.push_back(std::make_unique<Gamepad>(i));
+	}
+}
+
+
+//-----------------------------------------------------------------
+// Destructor
+//-----------------------------------------------------------------
+
+
+//-----------------------------------------------------------------
+// Public Member Functions
+//-----------------------------------------------------------------
+bool InputManager::ProcessInput()
 {
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
 			return false;
 		}
-		if (e.type == SDL_KEYDOWN) {
-			
-		}
-		if (e.type == SDL_MOUSEBUTTONDOWN) {
-			
-		}
-		// etc...
-
+		
 		//process event for ImGui
 		ImGui_ImplSDL2_ProcessEvent(&e);
 	}
 
+	m_Keyboard->Update();
+	for (const auto& controller : m_Gamepads)
+	{
+		controller->Update();
+	}
+
+	HandleKeyboardInput();
+	HandleGamepadInput();
+
 	return true;
 }
+
+void InputManager::AddKeyboardCommand(std::unique_ptr<Command> pCommand, const InputKeyboardBinding& binding)
+{
+	if (pCommand == nullptr) return;
+
+	m_KeyboardCommands.push_back(KeyboardCommand{ binding, std::move(pCommand) });
+}
+
+void InputManager::AddGamepadCommand(std::unique_ptr<Command> pCommand, const InputGamepadBinding& binding)
+{
+	if (pCommand == nullptr) return;
+
+	m_GamepadCommands.push_back(GamepadCommand{ binding, std::move(pCommand) });
+}
+ 
+
+//-----------------------------------------------------------------
+// Private Member Functions
+//-----------------------------------------------------------------
+void InputManager::HandleKeyboardInput()
+{
+	for (const auto& command : m_KeyboardCommands)
+	{
+		switch (command.first.inputState)
+		{
+		case InputState::Pressed:
+			if (m_Keyboard->IsPressed(command.first.key))
+				command.second->Execute();
+			break;
+		case InputState::Released:
+			if (m_Keyboard->IsReleased(command.first.key))
+				command.second->Execute();
+			break;
+		case InputState::Active:
+			if (m_Keyboard->IsActive(command.first.key))
+				command.second->Execute();
+			break;
+		}
+	}
+}
+
+void InputManager::HandleGamepadInput()
+{
+	for (const auto& command : m_GamepadCommands)
+	{
+		switch (command.first.inputState)
+		{
+		case InputState::Pressed:
+			if (m_Gamepads[static_cast<int>(command.first.controllerId)]->IsPressed(command.first.button))
+				command.second->Execute();
+			break;
+		case InputState::Released:
+			if (m_Gamepads[static_cast<int>(command.first.controllerId)]->IsReleased(command.first.button))
+				command.second->Execute();
+			break;
+		case InputState::Active:
+			if (m_Gamepads[static_cast<int>(command.first.controllerId)]->IsActive(command.first.button))
+				command.second->Execute();
+			break;
+		}
+	}
+}
+
--- Minigin/InputManager.h
+++ Minigin/InputManager.h
@@ -1,12 +1,82 @@
 #pragma once
+// Includes
+#include <vector>
+#include <memory>
 #include "Singleton.h"
+#include "Keyboard.h"
+#include "Gamepad.h"
+#include "Command.h"
 
 namespace dae
 {
+	// Class Forward Declarations
+	enum class InputState : unsigned char
+	{
+		Pressed,
+		Released,
+		Active,
+	};
+	enum class ControllerID : unsigned char
+	{
+		One		= 0,
+		Two		= 1,
+		Three	= 2,
+		Four	= 3,
+	};
+	struct InputKeyboardBinding
+	{
+		Keyboard::Key key;
+		InputState inputState;
+	};
+	struct InputGamepadBinding
+	{
+		Gamepad::Button button;
+		InputState inputState;
+		ControllerID controllerId;
+	};
+
+	// Class Declaration
 	class InputManager final : public Singleton<InputManager>
 	{
 	public:
+		// Constructors and Destructor
+		~InputManager() = default;
+		
+		// Copy and Move semantics
+		InputManager(const InputManager& other)					= delete;
+		InputManager& operator=(const InputManager& other)		= delete;
+		InputManager(InputManager&& other) noexcept				= delete;
+		InputManager& operator=(InputManager&& other) noexcept	= delete;
+		
+		//---------------------------
+		// Public Member Functions
+		//---------------------------
 		bool ProcessInput();
+
+		void AddKeyboardCommand(std::unique_ptr<Command> pCommand, const InputKeyboardBinding& binding);
+		void AddGamepadCommand(std::unique_ptr<Command> pCommand, const InputGamepadBinding& binding);
+		
+		
+	private:
+		// Member variables
+		std::unique_ptr<Keyboard> m_Keyboard{};
+		std::vector<std::unique_ptr<Gamepad>> m_Gamepads{};
+
+		using KeyboardCommand = std::pair<InputKeyboardBinding, std::unique_ptr<Command>>;
+		std::vector<KeyboardCommand> m_KeyboardCommands{};
+
+		using GamepadCommand = std::pair<InputGamepadBinding, std::unique_ptr<Command>>;
+		std::vector<GamepadCommand> m_GamepadCommands{};
+		
+		//---------------------------
+		// Private Member Functions
+		//---------------------------
+		friend class Singleton<InputManager>;
+		InputManager();
+
+		void HandleKeyboardInput();
+		void HandleGamepadInput();
+		
 	};
 
 }
