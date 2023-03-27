33c10504c0a4a8a1e541f3d87adea13e42adbc10
Jolan Plaum <jolan.plaum@student.howest.be>
2023-03-27 14:01:44 +0200

    Add base Command class without member variables

--- Minigin/Command.h
+++ Minigin/Command.h
@@ -2,8 +2,6 @@
 
 namespace dae
 {
-	class GameObject;
-
 	//=============================
 	// Base Command Class
 	//=============================
@@ -11,7 +9,7 @@ namespace dae
 	{
 	public:
 		// Constructors and Destructor
-		explicit Command(GameObject* pGameObject) :m_pGameObject{ pGameObject } {}
+		explicit Command() = default;
 		virtual ~Command() = default;
 		
 		// Copy and Move semantics
@@ -22,6 +20,28 @@ namespace dae
 
 		// Public Member Functions
 		virtual void Execute() = 0;
+	};
+
+
+	//=============================
+	// GameObject Command Class
+	//=============================
+	class GameObject;
+	class GameObjectCommand : public Command
+	{
+	public:
+		// Constructors and Destructor
+		explicit GameObjectCommand(GameObject* pGameObject) :m_pGameObject{ pGameObject } {}
+		virtual ~GameObjectCommand() = default;
+		
+		// Copy and Move semantics
+		GameObjectCommand(const GameObjectCommand& other)					= delete;
+		GameObjectCommand& operator=(const GameObjectCommand& other)		= delete;
+		GameObjectCommand(GameObjectCommand&& other) noexcept				= delete;
+		GameObjectCommand& operator=(GameObjectCommand&& other) noexcept	= delete;
+
+		// Public Member Functions
+		virtual void Execute() override = 0;
 
 
 	private:
