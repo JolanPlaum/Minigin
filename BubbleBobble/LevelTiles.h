3697e55e4b1dc8fc7866fdb2681cf781651132e1
Jolan Plaum <jolan.plaum@student.howest.be>
2023-08-17 15:57:05 +0200

    Create LevelTiles prefab

---
+++ BubbleBobble/LevelTiles.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "Scene.h"
+#include "GameObject.h"
+#include "ResourceManager.h"
+#include "CSpriteRenderer.h"
+#include "BoxCollider2D.h"
+
+namespace dae
+{
+	GameObject* LevelTileBig(Scene& scene, unsigned int level)
+	{
+		// Create object
+		GameObject* pGo = scene.CreateObject();
+		pGo->SetTag("StaticLevel");
+
+		// Add components
+		auto pRenderer = pGo->AddComponent<CSpriteRenderer>();
+		auto pCollider = pGo->AddComponent<BoxCollider2D>();
+
+		// Alter components
+		pRenderer->SetSprite(ResourceManager::GetInstance().LoadSprite("BubbleBobble/Sprites/LevelTilesBig.png"));
+		pRenderer->SetSettings(SpriteRenderSettings::IterateNone);
+		pRenderer->SetTileIdx(level);
+		pCollider->SetSize(pRenderer->GetSize());
+
+		// Return object
+		return pGo;
+	}
+
+	GameObject* LevelTileSmall(Scene& scene, unsigned int level)
+	{
+		// Create object
+		GameObject* pGo = scene.CreateObject();
+		pGo->SetTag("Platform");
+
+		// Add components
+		auto pRenderer = pGo->AddComponent<CSpriteRenderer>();
+		auto pCollider = pGo->AddComponent<BoxCollider2D>();
+
+		// Alter components
+		pRenderer->SetSprite(ResourceManager::GetInstance().LoadSprite("BubbleBobble/Sprites/LevelTilesSmall.png"));
+		pRenderer->SetSettings(SpriteRenderSettings::IterateNone);
+		pRenderer->SetTileIdx(level);
+		pCollider->SetSize(pRenderer->GetSize());
+
+		// Return object
+		return pGo;
+	}
+}
